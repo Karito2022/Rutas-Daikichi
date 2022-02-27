57488f914331d28674a6c38376554e308002dc37
Karito2022 <reyesbarrerakaren@gmail.com>
2022-02-26 23:34:48 -0500

    Daikichi comit

---
+++ RutasDaikichi/src/main/java/com/daikichi/project/RutasDaikichiApplication.java
@@ -0,0 +1,16 @@
+package com.daikichi.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RutasDaikichiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutasDaikichiApplication.class, args);
+	}
+}
---
+++ RutasDaikichi/src/main/java/com/daikichi/project/controladores/ControladorSaludos.java
@@ -0,0 +1,25 @@
+package com.daikichi.project.controladores;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControladorSaludos {
+	@RestController
+	@RequestMapping("/daikichi")
+	public class HomeController {
+	    @RequestMapping("")
+	    public String index(){
+	      return "Welcome!";
+	    }
+	    @RequestMapping("/today")
+	    public String today(){
+	      return "Today you will find luck in all your endeavors!";
+	    }
+	    @RequestMapping("/tomorrow")
+	    public String tomorrow(){
+	      return "Tomorrow,an opportunity will arise, so be sure to be open to new ideas!";
+	    }
+	}
+
+}
